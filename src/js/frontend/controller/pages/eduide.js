e5f4bdc425cb18ece1175370969344f53ee81f07
Mark James Ivanowich <mark@ivanowich.ca>
2018-04-27 10:22:37 -0500

    eduide cleanup/prep

--- src/js/frontend/controller/pages/eduide.js
+++ src/js/frontend/controller/pages/eduide.js
@@ -9,33 +9,14 @@ class Eduide {
 
 	init() {
 		this.container = jQuery('.edx-page');
-		this.template();
 		this.editor();
 		this.courses();
 	}
 
-	template() {
-		let container, content;
-		container = this.container;
-		content = '<div class="edx-angled-sections"></div>'
-		container.html(content);
-		this.sections();
-	}
-
-	sections() {
-		let container, content, sections;
-		container = jQuery('.edx-angled-sections');
-		sections = ['editor','courses'];
-	    for (var i = 0; i < sections.length; i++) {
-	    	content = `<div class="edx-section edx-angled-section edx-section-`+sections[i]+`"></div>`;
-	    	container.append(content);
-	    }
-	}
-
 	editor() {
 		let container, content, code, editor = new Editor();
-		container = jQuery('.edx-section-editor');
-		content = 	`<div class="edx-angled-section-wrapper edx-container">
+		container = this.container;
+		content = 	`<div class="edx-container">
 						<div class="edx-editor-container">
 							<div class="edx-wrapper edx-editor-wrapper">
 								<div class="edx-xs-100 edx-sm-100 edx-md-75 edx-lg-75">
@@ -91,56 +72,5 @@ class Eduide {
 		});
 	}
 
-	courses() {
-		let container, content;
-		container = jQuery('.edx-section-courses');
-		content = 	`<div class="edx-angled-section-wrapper edx-container">
-						<div class="edx-wrapper">
-							<div class="edx-xs-100 edx-sm-100 edx-md-100 edx-lg-100">
-								<div class="edx-section-title">Most recent courses</div>
-							</div>
-							<div class="edx-xs-100 edx-sm-50 edx-md-33 edx-lg-33">
-								<div class="edx-course-card edx-course-card-javascript">
-									<div class="edx-course-card-cover edx-wrapper">
-										<div class="edx-course-card-cover-titles">
-											<div class="edx-course-card-cover-title">javascript</div>
-											<div class="edx-course-card-cover-subtitle">console logging</div>
-										</div>
-									</div>
-									<div class="edx-course-card-content">
-										<div class="edx-course-card-description">Lorem ipsum dolor sit amet, no sit sonet corpora indoctum, quo ad fierent insolens. Duo aeterno ancillae ei.</div>
-									</div>
-								</div>
-							</div>
-							<div class="edx-xs-100 edx-sm-50 edx-md-33 edx-lg-33">
-								<div class="edx-course-card edx-course-card-html">
-									<div class="edx-course-card-cover edx-wrapper">
-										<div class="edx-course-card-cover-titles">
-											<div class="edx-course-card-cover-title">html</div>
-											<div class="edx-course-card-cover-subtitle">headings and paragraphs</div>
-										</div>
-									</div>
-									<div class="edx-course-card-content">
-										<div class="edx-course-card-description">Lorem ipsum dolor sit amet, no sit sonet corpora indoctum, quo ad fierent insolens. Duo aeterno ancillae ei.</div>
-									</div>
-								</div>
-							</div>
-							<div class="edx-xs-100 edx-sm-50 edx-md-33 edx-lg-33">
-								<div class="edx-course-card edx-course-card-php">
-									<div class="edx-course-card-cover edx-wrapper">
-										<div class="edx-course-card-cover-titles">
-											<div class="edx-course-card-cover-title">php</div>
-											<div class="edx-course-card-cover-subtitle">comparing strings</div>
-										</div>
-									</div>
-									<div class="edx-course-card-content">
-										<div class="edx-course-card-description">Lorem ipsum dolor sit amet, no sit sonet corpora indoctum, quo ad fierent insolens. Duo aeterno ancillae ei.</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>`;
-		container.html(content);
-	}
 
 }
