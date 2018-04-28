726da11ff8151f512722dd5a9355314256946232
Mark James Ivanowich <mark@ivanowich.ca>
2018-04-27 19:13:20 -0500

    starting to refactor Editor for tabs/file support

--- src/js/frontend/controller/core/editor.js
+++ src/js/frontend/controller/core/editor.js
@@ -1,32 +1,48 @@
 class Editor {
-
 	init(data) {
 		this.data = data;
-		this.build();
+		//set editor defaults, if they don't exist.
+		//be explicit: don't use logical OR for defining defaults
+		this.defaultTheme = data.theme===undefined?'ace/theme/monokai':data.theme;
+		this.defaultMode = data.mode===undefined?'ace/mode/html':data.mode;
+		this.defaultWrap = data.wrap===undefined?true:data.wrap;
+		this.defaultMargin = data.margin===undefined?false:data.margin;
+		this.defaultReadonly = data.readonly===undefined?false:data.readonly;
+
+		let container = jQuery("#"+data.id);
+
+		this.tabs = [];
+		// this.build();
+		if($.trim(container.html())==''){
+			this.newTab();
+			if(data.shadow) jQuery('.edx-editor-holder').addClass('shadow');
+		}else{
+			container.empty();
+			if(data.shadow) jQuery('.edx-editor-holder').removeClass('shadow');
+		}
 	}
 
-	build() {
-		let data, editor, length;
-		data = this.data;
-		if(data.code) {
-			let code;
-			code = data.code.replace(/\</g,"&lt;");
-			code = code.replace(/\>/g,"&gt;");
-			jQuery('#'+data.id).html(code);
-		}
+	build(data) {
+
+		let editor, length;
+		if(data.id===){}else{
 		editor = ace.edit(data.id);
-		editor.setTheme(data.theme);
-		editor.session.setMode(data.mode);
-		editor.session.setUseWrapMode(data.wrap);
-		editor.setShowPrintMargin(data.margin);
-		if(data.shadow) {
-			jQuery('.edx-editor-holder').addClass('shadow');
-		}
+		editor.setTheme(data.theme===undefined?this.defaultTheme:data.theme);
+		editor.session.setMode(data.mode===undefined?this.defaultMode:data.mode);
+		editor.session.setUseWrapMode(data.wrap===undefined?this.defaultWrap:data.wrap);
+		editor.setShowPrintMargin(data.margin===undefined?this.defaultMargin:data.margin);
+		editor.setReadonly(data.margin===undefined?this.defaultMargin:data.margin);
 		if(data.focus) {
 			length = editor.session.getLength();
 			editor.gotoLine(length);
 			editor.focus();
 		}
+		document.aces = editor;
+		}
+	}
+
+	newTab(){
+		this.build();
 	}
 
 }
